00cf9535f5efc8dec108fbbd947eb3581b439c5b
Ali Haider <ali74576@gmail.com>
2020-08-06 13:00:18 +0500

    first version

---
+++ Dashboard 1.0.0/InstaSS/Dashboard.xaml.cs
@@ -0,0 +1,54 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dashboard_1._0._0.InstaSS
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : UserControl
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+            SeriesCollection = new SeriesCollection
+            {
+                new StackedColumnSeries
+                {
+                    Values = new ChartValues<double> {24, 52, 61, 89},
+                    StackMode = StackMode.Values,
+                    DataLabels = true
+                },
+                new StackedColumnSeries
+                {
+                    Values = new ChartValues<double> {-15, -25, -16, -11},
+                    StackMode = StackMode.Values,
+                    DataLabels = true
+                }
+            };
+
+            Labels = new[] { "Feb 7", "Feb 8", "Feb 9", "Feb 10" };
+            Formatter = value => value.ToString();
+
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
+    }
+}
---
+++ Dashboard 1.0.0/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dashboard_1._0._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void LeftMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = LeftMenu.SelectedIndex;
+            ChangeSelection(index);
+        }
+        private void ChangeSelection(int index)
+        {
+            for(int i= (LeftMenu.Items.Count -1); i >= 0; i--)
+            {
+                if(i == index)
+                {
+                    ListViewItem focusListItem = (ListViewItem)(LeftMenu.ItemContainerGenerator.ContainerFromIndex(i));
+                    focusListItem.Foreground = new SolidColorBrush(Color.FromRgb(255,255,255));
+                    focusListItem.Background = new SolidColorBrush(Color.FromRgb(233, 67, 64));
+                }
+                else
+                {
+                    ListViewItem focusListItem = (ListViewItem)(LeftMenu.ItemContainerGenerator.ContainerFromIndex(i));
+                    focusListItem.Foreground = new SolidColorBrush(Color.FromRgb(128, 128, 128));
+                    focusListItem.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                }
+            }
+        }
+    }
+}
